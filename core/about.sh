1f8da882a93a3eb53c111d51034df7392a4855d9
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-01-02 00:15:31 +0300

    Create about.sh

---
+++ core/about.sh
@@ -0,0 +1,31 @@
+clear
+
+echo '                    
+
+				/¯\ |¯/ /¯\ | | ¯|¯ 				|¯| |¯/ |¯| |_|  |  
+
+				     ¯                v3.0
+
+                                                 
+
+' | lolcat
+
+echo " "
+
+echo "                             About"|lolcat
+
+echo " "
+
+echo " hi i'm ReKuShE dev this tool the tool for hack insta use attack pass and without block"| lolcat
+
+echo ""
+
+echo "                  Our YouTube  :- Bs4"| lolcat
+
+echo " "
+
+sleep 5.0
+
+cd $HOME/hack-instagram
+
+bash bs4-insta.sh
