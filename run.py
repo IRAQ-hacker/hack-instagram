53c2bc6064f5975a3d0a6277fce9c9d9ef70d5cf
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-01-02 21:02:58 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
 import os,sys,time
-print('please white second ')
-time.sleep(0.6)
-os.system('xdg-open https://youtube.com/channel/UCCgmIKpPgUOQauZ3IvrchBA')
+
+print"""
+\033[1;91m1- YouTube. 
+\033[1;91m12- github .
+\033[1;91m13- telegram account .
+\033[1;91m14- channel telegram .
+"""
+xp = input('type number : ")
+if xp =='1':
+
+    print('please white second and subscribe in YouTube for run the tool')
+    time.sleep(0.6)
+    os.system('xdg-open https://youtube.com/channel/UCCgmIKpPgUOQauZ3IvrchBA')
+if xp =='2':
+    print('please white second and subscribe in YouTube for run the tool')
+    time.sleep(0.6)
+    os.system('xdg-open https://github.com/iraq-hacker')
+if xp =='3':
+    print('white second  please ')
+    os.system('xdg-open https://t.me/iiwiw')
+           
+if xp =='4':
+    print('white second  please ')
+    os.system('xdg-open https://t.me/professional_school')
