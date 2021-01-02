1978489185f4ca699d3f24afdaa6b7a4463a1488
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-01-02 20:28:56 +0300

    Update bs4-insta.sh

--- bs4-insta.sh
+++ bs4-insta.sh
@@ -31,10 +31,10 @@ printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Bs4  \e[100m\e[1;77m <<
 printf "\n"
 echo 
 
-printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Auto Attack\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Auto list in tool  Attack\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m With list Attack\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m About\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Update\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m About tool \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Update the tool\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
 
@@ -56,7 +56,7 @@ exit 1
 
 else
 echo
-printf "\e[1;93m [>!<] Invalid Selection!\e[0m\n"
+printf "\e[1;93m [>!<] Invalid !\e[0m\n"
 sleep 1
 echo
 fi
