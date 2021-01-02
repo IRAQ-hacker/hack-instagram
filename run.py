ab00e27f2aea6b8108856f12201e2b8de65347ef
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-01-02 21:57:52 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,47 @@
+import os,sys,time
+
+os.system("clear")
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.08)
+
+jalan ("\033[1;91m===10%")
+os.system("clear")
+jalan ("\033[1;92m======40%")
+os.system("clear")
+jalan ("\033[1;93m===========60%")
+os.system("clear")
+jalan ("\033[1;94m===================80%")
+os.system("clear")
+jalan ("\033[1;95m==========================100%")
+os.system("clear")
+print("""
+\033[1;91m1- YouTube. 
+\033[1;91m12- github .
+\033[1;91m13- telegram account .
+\033[1;91m14- channel telegram .
+""")
+xp = input('type number : ')
+if xp =='1':
+
+    print('please white second and subscribe in YouTube for run the tool')
+    time.sleep(0.6)
+    os.system('xdg-open https://youtube.com/channel/UCCgmIKpPgUOQauZ3IvrchBA')
+           
+if xp =='2':
+           
+    print('please white second and subscribe in YouTube for run the tool')
+    os.system('xdg-open https://github.com/iraq-hacker')
+           
+if xp =='3':
+           
+    print('white second  please ')
+    os.system('xdg-open https://t.me/iiwiw')
+           
+if xp =='4':
+           
+    print('white second  please ')
+    os.system('xdg-open https://t.me/professional_school')
